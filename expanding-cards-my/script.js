f179045f857eee2eb9fcda88c3f0f8bd10273603
wuwenjun08 <stevewuwen@qq.com>
2025-10-31 20:01:27 +0800

    1

--- expanding-cards-my/script.js
+++
@@ -1,8 +0,0 @@
-containers = document.getElementsByClassName('image-container')
-
-for (let index = 0; index < containers.length; index++) {
-    const element = containers[index];
-    element.addEventListener('click', ()=>{
-        element.classList.toggle("container-activate")
-    })
-}
